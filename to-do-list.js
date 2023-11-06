008cea22fcdabd0a3b6d5b1e81e773369f556f03
chentiwuni <chienntiwuni@gmail.com>
2023-11-06 15:49:32 +0000

    added an edit function

--- to-do-list.js
+++ to-do-list.js
@@ -1,46 +1,60 @@
-const inputDisplay = document.getElementById('inputBox');
+const inputField = document.getElementById('inputField');
 const listContainer = document.getElementById('listContainer');
+let editMode = false;
+let taskToEdit = null;
 
 function addTask() {
-    if(inputDisplay.value === "") {
-        alert('you must write a task')
+    if (inputField.value === "") {
+        alert('You must write a task');
     } else {
-
-        let li = document.createElement("li");
-        li.innerHTML = inputDisplay.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.className = "closeTask"
-        span.innerHTML = "\u2715";
-        li.appendChild(span);
-        li.style.display = "block";
-
-       
+        if (editMode) {
+            taskToEdit.firstChild.textContent = inputDisplay.value; // Update the text content of the first child (the text node) of the <li>
+            editMode = false;
+            taskToEdit = null;
+        } else {
+            let li = document.createElement("li");
+            li.innerHTML = inputField.value;
+            listContainer.appendChild(li);
+            let span0 = document.createElement("span");
+            span0.className = "edit";
+            span0.innerHTML = "\u270F";
+            li.appendChild(span0);
+            let span = document.createElement("span");
+            span.className = "closeTask";
+            span.innerHTML = "\u2715";
+            li.appendChild(span);
+            li.style.display = "block";
+        }
     }
 
-    inputDisplay.value = "";
-
+    inputField.value = "";
     saveData();
 }
-listContainer.addEventListener('click', function(e) {
+
+listContainer.addEventListener('click', function (e) {
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData();
-    } 
-      else if (e.target.tagName === "SPAN") {
+        if (!editMode) {
+            e.target.classList.toggle("checked");
+            saveData();
+        }
+    } else if (e.target.className === "closeTask") {
         const confirmation = confirm("Are you sure you want to remove this task?");
         if (confirmation) {
             e.target.parentElement.remove();
             saveData();
         }
-        }
+    } else if (e.target.className === "edit") {
+        editMode = true;
+        taskToEdit = e.target.parentElement;
+        inputField.value = taskToEdit.firstChild.textContent; // Set the input field value to the text content of the <li>
+    }
 }, false);
 
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showData () {
+function showData() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
 
